bd88ab9e53d700ff41c841f111a392bb0c231d62
Ivan Kitaev <vanya.kitaev.7777@gmail.com>
2023-04-10 11:53:08 +0300

    Классы

--- Task 4/index.js
+++ Task 4/index.js
@@ -8,9 +8,9 @@ class Square extends Quadrilateral{
     constructor(a){
         super(a)
     }
-    // squareSquare(){
-    //     return Math.pow(this.a,2)
-    // }
+    calcSquare(){
+        return Math.pow(this.a,2)
+    }
 }
 
 class Rectangle extends Quadrilateral{
@@ -18,32 +18,24 @@ class Rectangle extends Quadrilateral{
         super(a)
         this.b =b
     }
-    // squareRectangle(){
-    //     return this.a * this.b
-    // }
+    calcSquare(){
+        return this.a * this.b
+    }
 }
 
 class Circle{
     constructor(r){
         this.r = r 
     }
-    // squareCircle(){
-    //     return Math.PI *(Math.pow(this.r,2))
-    // }
+    calcSquare(){
+        return Math.PI *(Math.pow(this.r,2))
+    }
 }
 
 function totalSquare(array){
-    let sum = 0
-    var squareSqu = array.shift(squ)
-    //console.log(squareSqu)
-    var squareRec = array.shift(rec)
-    //console.log(squareRec)
-    var squareCir = array.shift(cir)
-    //console.log(squareCir)
-    let resSqu = Math.pow(squareSqu.a,2)
-    let resRec = squareRec.a * squareRec.b
-    let resCir = Math.PI *(Math.pow(squareCir.r,2))
-    return sum = resSqu+resRec+resCir
+    var sum = 0
+    array.map(element => sum+=element.calcSquare())
+    return sum;
 }
 
 const squ = new Square(5)
